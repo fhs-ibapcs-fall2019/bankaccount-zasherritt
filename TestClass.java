25f640d231c8aed2587cecec56f38aef9c908400
Tim Gesell <timothy.gesell@bvsd.org>
2019-08-21 12:43:54 -0600

    Starter Files for Project

---
+++ TestClass.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class TestClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TestClass
+{
+    public static void main(String[] args)
+    {
+        BankAccount a1 = new BankAccount(500, "gesell", 123, "pass");
+        a1.balance = 1000000;
+        a1.password = "newpass";
+        
+    }
+}
